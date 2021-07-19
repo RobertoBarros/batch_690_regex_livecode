26bd2768b36c795d9894cd6dd2b58ac250f785c7
Roberto Barros <roberto.f.barros@gmail.com>
2021-07-19 17:49:18 -0300

    livecode

--- ssn.rb
+++ ssn.rb
@@ -1,6 +1,6 @@
 require 'date'
 
-PATTERN = //
+PATTERN = /^(?<gender>[1-2])\s?(?<year>\d{2})\s?(?<month>[01][0-9])\s?(?<department>\d{2})/
 
 DEPARTMENTS = {
   '75' => "Paris",
@@ -9,13 +9,24 @@ DEPARTMENTS = {
 }
 
 def ssn_info(ssn)
-  return '????'
+  match = ssn.match(PATTERN)
 
+  gender = match[:gender] == '1' ? 'A man' : 'A woman'
+  year = match[:year]
+  month = Date::MONTHNAMES[match[:month].to_i]
+  department = DEPARTMENTS[match[:department]]
+
+  return "#{gender}, born in #{month}, #{year} in #{department}"
   # return => "a man, born in December, 1984 in Seine-Maritime."
 end
 
 def ssn_valid?(ssn)
-  return false
+  ssn_without_key = ssn.gsub(/\s/, '')[0..-3].to_i
+  key = ssn.gsub(/\s/, '')[13..14].to_i
+  
+  #  remainder of the division of (97 - ssn_without_key) by 97.)
+  
+  return (97 - ssn_without_key) % 97 == key
 end
 
 
@@ -26,6 +37,8 @@ numbers = [
   "123",
   "6 86 10 75 114 511 75"
 ]
+
+
 numbers.each do |number|
   if ssn_valid?(number)
     puts "#{number} => #{ssn_info(number)}"
